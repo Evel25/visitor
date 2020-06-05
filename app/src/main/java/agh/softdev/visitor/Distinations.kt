0ae0a4a9ac9d848af98d99cc800cccb3f784633d
Evel25 <bi3.h300@gmail.com>
2020-06-05 09:27:16 +0100

    set up the layout and the app skeleton

---
+++ app/src/main/java/agh/softdev/visitor/Distinations.kt
@@ -0,0 +1,12 @@
+package agh.softdev.visitor
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class Distinations : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_distinations)
+    }
+}
